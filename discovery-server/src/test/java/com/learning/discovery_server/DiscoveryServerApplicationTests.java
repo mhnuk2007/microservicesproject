d116e5e678def88665be3fbd84f572b2dd181456
Mohan Lal <mhnuk2007@gmail.com>
2025-09-27 10:03:26 +0500

    Add discovery-server project source

---
+++ discovery-server/src/test/java/com/learning/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
